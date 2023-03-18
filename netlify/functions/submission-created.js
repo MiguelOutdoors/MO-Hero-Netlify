40310a2fb7bf4e324786aa8dadecf91db52018db
Miguel Angel <mlopezri95@gmail.com>
2023-03-18 10:26:39 -0400

    reverting code. I just gotta pay smh

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,27 +1,24 @@
-require('dotenv').config()
-const fetch = require('node-fetch')
-const { BUTTONDOWN_API_KEY } = process.env
+require("dotenv").config();
+const fetch = require("node-fetch");
+const { BUTTONDOWN_API_KEY } = process.env;
 
-exports.handler = async event => {
-const payload = JSON.parse(event.body).payload
-console.log(`Received a submission: ${payload.email}`)
+exports.handler = async (event) => {
+  const payload = JSON.parse(event.body).payload;
+  console.log(`Received a submission: ${payload.email}`);
 
-const response = await fetch(
-  'https://api.buttondown.email/v1/subscribers',
-  {
-    method: 'POST',
+  const response = await fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
     headers: {
       Authorization: `Token ${BUTTONDOWN_API_KEY}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ email: payload.email }),
-  }
-);
+  });
 
-let responseText = await response.text();
-console.log('response:', responseText);
-return {
-  statusCode: response.status,
-  body: responseText,
+  let responseText = await response.text();
+  console.log("response:", responseText);
+  return {
+    statusCode: response.status,
+    body: responseText,
+  };
 };
-}
